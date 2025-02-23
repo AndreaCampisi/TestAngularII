6d797f59c4af8dcc96ae7f0bbfb95896f29bbb09
Andrea Campisi <ilcampo94@gmail.com>
2025-02-23 21:39:39 +0100

    add some comment in investment result component

--- src/app/investment/investment-result/investment-result.component.ts
+++ src/app/investment/investment-result/investment-result.component.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from './investment.service';
 
 @Component({
   selector: 'app-investment-result',
-  standalone:true,
+  standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
-  styleUrl: './investment-result.component.css'
+  styleUrl: './investment-result.component.css',
 })
 export class InvestmentResultComponent {
   private investmentService = inject(InvestmentService);
   // we use get here to make sure that the investmentResults is available in the template
-  get investmentResults() {
-    return this.investmentService.investmentResults;
-  }
+  // get investmentResults() {
+  //   return this.investmentService.investmentResults;
+  // }
+
+  //we use computed here to make sure that the investmentResults is not modified outside the service
+  //because computed provides a read-only value
+  investmentResults = computed(() =>
+    this.investmentService.investmentResults()
+  );
+  //you can also use the following code to make sure that the investmentResults is not modified outside the service
+  //investmentResults = this.investmentService.investmentResults.asReadonly();
 }
